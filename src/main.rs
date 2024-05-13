898f0411b33fb86ee613bc9c03dbf6e3df8cb356
Fadenfire <13701628+Fadenfire@users.noreply.github.com>
2024-05-13 10:11:29 -0500

    Add basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() {
+	setup_logging();
+	
+	info!("Starting server");
+	
+	
+}
+
+fn setup_logging() {
+	let subscriber = FmtSubscriber::builder()
+		.with_max_level(Level::INFO)
+		.finish();
+	
+	tracing::subscriber::set_global_default(subscriber)
+		.expect("setting default subscriber failed");
 }
