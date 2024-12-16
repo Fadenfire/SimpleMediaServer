6ac78a35ef78a3b6fa1d9532f16de2bb39f72487
Fadenfire <13701628+Fadenfire@users.noreply.github.com>
2024-12-16 14:05:58 -0600

    Always show back button

--- web-ui/src/routes/(app)/+layout.svelte
+++ web-ui/src/routes/(app)/+layout.svelte
@@ -9,9 +9,8 @@
 	<a class="nav-item" href="/"><FeatherIcon name="home" size="100%"/></a>
 	<a class="nav-item" href="/watch_history"><FeatherIcon name="clock" size="100%"/></a>
 	
-	<!-- {#if isStandalone()}
-		<a class="nav-item" href="javascript:window.history.back()"><FeatherIcon name="arrow-left" size="100%"/></a>
-	{/if} -->
+	<!-- svelte-ignore a11y-invalid-attribute -->
+	<a class="nav-item" href="javascript:window.history.back()"><FeatherIcon name="arrow-left" size="100%"/></a>
 	
 	<div style="flex: 1;"></div>
 	
