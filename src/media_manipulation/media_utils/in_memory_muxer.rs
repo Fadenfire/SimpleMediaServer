2d9547f46b02929eb0192d040fd7ff0c80e97cf8
Fadenfire <13701628+Fadenfire@users.noreply.github.com>
2026-06-12 10:06:25 -0500

    Improve in memory muxer to use avio_open_dyn_buf instead of a custom system

--- src/media_manipulation/media_utils/in_memory_muxer.rs
+++ src/media_manipulation/media_utils/in_memory_muxer.rs
@@ -1,71 +1,61 @@
 use std::ffi::CString;
 use std::ops::{Deref, DerefMut};
-use std::ptr::{null_mut, slice_from_raw_parts};
 use std::ptr;
-
+use std::ptr::null_mut;
+use anyhow::anyhow;
 use ffmpeg_next::format;
-use ffmpeg_sys_next::{av_free, av_malloc, AVERROR_EOF, AVFMT_FLAG_CUSTOM_IO, avformat_alloc_output_context2, avio_alloc_context};
-use turbojpeg::libc;
+use ffmpeg_sys_next::{av_free, av_write_frame, avformat_alloc_output_context2, avio_close_dyn_buf, avio_open_dyn_buf};
 
 use crate::media_manipulation::media_utils::av_error;
 
 pub struct InMemoryMuxer {
 	output: format::context::Output,
-	boxed_buffer: Box<Option<Vec<u8>>>,
 }
 
 impl InMemoryMuxer {
-	const BUFFER_SIZE: usize = 4096;
-	
 	pub fn new(format: &str) -> Result<Self, ffmpeg_next::Error> {
-		let mut boxed_buffer = Box::new(Some(Vec::new()));
-		
 		unsafe {
-			let box_ptr: *mut _ = boxed_buffer.as_mut();
 			let mut ctx = null_mut();
 			let format = CString::new(format).unwrap();
 			
 			av_error(avformat_alloc_output_context2(&mut ctx, ptr::null(), format.as_ptr(), ptr::null()))?;
-			
-			let buffer = av_malloc(Self::BUFFER_SIZE);
-			
-			let avio = avio_alloc_context(
-				buffer.cast(), Self::BUFFER_SIZE as _,
-				1,
-				box_ptr.cast(),
-				None,
-				Some(Self::write_packet),
-				None,
-			);
-			
-			(*ctx).pb = avio;
-			(*ctx).flags |= AVFMT_FLAG_CUSTOM_IO;
+			av_error(avio_open_dyn_buf(&mut (*ctx).pb))?;
 			
 			let output = format::context::Output::wrap(ctx);
 			
-			Ok(Self {
-				output,
-				boxed_buffer,
-			})
+			Ok(Self { output })
 		}
 	}
 	
-	pub fn into_output_buffer(mut self) -> Vec<u8> {
-		self.boxed_buffer.take().expect("Already taken")
+	pub fn flush(&mut self) -> Result<(), ffmpeg_next::Error> {
+		unsafe {
+			av_error(av_write_frame(self.output.as_mut_ptr(), null_mut()))?;
+		}
+		
+		Ok(())
 	}
 	
-	unsafe extern "C" fn write_packet(opaque: *mut libc::c_void, buf_ptr: *const u8, buf_size: libc::c_int) -> libc::c_int {
-		let output_buffer_ref: *mut Option<Vec<u8>> = opaque.cast();
+	pub fn drain_output_buffer(&mut self) -> anyhow::Result<Vec<u8>> {
+		self.flush()?;
 		
 		unsafe {
-			if let Some(ref mut output_buffer) = *output_buffer_ref {
-				let buf = &*slice_from_raw_parts(buf_ptr, buf_size as usize);
-				output_buffer.extend_from_slice(buf);
-				
-				buf_size
-			} else {
-				AVERROR_EOF
+			let ctx = self.output.as_mut_ptr();
+			
+			let mut av_buffer = null_mut();
+			let size = avio_close_dyn_buf((*ctx).pb, &mut av_buffer);
+			
+			if av_buffer.is_null() {
+				return Err(anyhow!("avio_close_dyn_buf returned null buffer"));
 			}
+			
+			let av_buffer = scopeguard::guard(av_buffer, |ptr| av_free(ptr.cast()));
+			
+			(*ctx).pb = null_mut();
+			av_error(avio_open_dyn_buf(&mut (*ctx).pb))?;
+			
+			let buffer = std::slice::from_raw_parts(*av_buffer, size as usize).to_vec();
+			
+			Ok(buffer)
 		}
 	}
 }
@@ -87,11 +77,15 @@ impl DerefMut for InMemoryMuxer {
 impl Drop for InMemoryMuxer {
 	fn drop(&mut self) {
 		unsafe {
-			let avio = (*self.as_mut_ptr()).pb;
-			(*self.as_mut_ptr()).pb = null_mut();
+			let ctx = self.output.as_mut_ptr();
 			
-			av_free((*avio).buffer.cast());
-			av_free(avio.cast());
+			let avio = (*ctx).pb;
+			(*ctx).pb = null_mut();
+			
+			let mut av_buffer = null_mut();
+			avio_close_dyn_buf(avio, &mut av_buffer);
+			
+			av_free(av_buffer.cast());
 		}
 	}
 }
